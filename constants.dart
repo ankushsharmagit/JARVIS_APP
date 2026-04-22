d71896b6b5f671dfbe6f152f004ca6a22f48aa9f
ankushsharmagit <ankushtext1@gmail.com>
2026-04-22 19:46:19 +0530

    lib/config/

---
+++ constants.dart
@@ -0,0 +1,20 @@
+// lib/config/constants.dart
+class AppConstants {
+  static const String appName = 'JARVIS';
+  static const String appVersion = '4.0.0';
+  static const String ownerName = 'Mukul';
+  
+  static const String geminiApiKey = 'AIzaSyB0XEtmp2JP-1NQOFhoWxJMM3YaKRyM3Kw';
+  static const String telegramBotToken = '8650822300:AAEe2r94dOGMBI9co7Lh5PKxuas8rjO_zvQ';
+  
+  static const List<String> wakeWords = [
+    'jarvis', 'hey jarvis', 'hello jarvis', 'ok jarvis', 'jarvis bhai'
+  ];
+}
+
+class JarvisColors {
+  static const int bgPrimary = 0xFF0A0E21;
+  static const int bgCard = 0xFF16213E;
+  static const int accentCyan = 0xFF00D4FF;
+  static const int success = 0xFF00FF88;
+}

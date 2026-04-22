3bb6e620795ed68d1408add876af7a24a696f929
ankushsharmagit <ankushtext1@gmail.com>
2026-04-22 19:49:42 +0530

    lib/config/

---
+++ colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class JarvisColors {
+  static const Color bgPrimary = Color(0xFF0A0E21);
+  static const Color bgSecondary = Color(0xFF1A1A2E);
+  static const Color bgCard = Color(0xFF16213E);
+  static const Color accentCyan = Color(0xFF00D4FF);
+  static const Color accentBlue = Color(0xFF0088FF);
+  static const Color success = Color(0xFF00FF88);
+  static const Color error = Color(0xFFFF0040);
+  static const Color warning = Color(0xFFFF8800);
+  static const Color textPrimary = Color(0xFFFFFFFF);
+  static const Color textSecondary = Color(0xFFB0BEC5);
+  static const Color textHint = Color(0xFF607D8B);
+  static const Color borderColor = Color(0x3300D4FF);
+  static const Color dividerColor = Color(0x1AFFFFFF);
+  static const Color chatUserBubble = Color(0xFF0088FF);
+  static const Color chatJarvisBubble = Color(0xFF16213E);
+}
